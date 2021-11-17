c488bcbfd61adb49a4b4fcb28160352c41708eb9
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-11-17 17:46:46 +0100

    Modele - barwiarz, piosenka, informacje

--- stronaK/models.py
+++ stronaK/models.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 # RafKac
-
+from django.conf import settings
 from django.db import models
 
 
-class Barwiarz(models.Model):
+class Knight(models.Model):
+    name = models.TextField(null=True)
+    last_name = models.TextField(null=False)
+    date_birth = models.IntegerField(null=True)
+    date_death = models.IntegerField(null=True)
+    comments = models.TextField()
 
 
-class Piosenka(models.Model):
+class Song(models.Model):
+    title = models.TextField(null=False)
+    text = models.TextField(null=False)
+    author = models.TextField(null=True)
+    date = models.TextField(null=True)
+    comments = models.TextField(null=True)
+
+
+class News(models.Model):
+    title = models.TextField(null=False)
+    text = models.TextField(null=False)
+    date = models.DateField(null=False)
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    picture = models.ImageField()
