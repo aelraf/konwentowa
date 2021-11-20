dcd8d1f6cc6df4bd3f275813b8a885cc072a2ab2
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-11-20 17:18:41 +0100

    zmiana widoków na podejście generyczne

--- stronaK/views.py
+++ stronaK/views.py
@@ -4,7 +4,18 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import Http404
 # from django.http import HttpResponse
-from stronaK.models import News
+from django.views import generic
+
+from stronaK.models import News, Song, OldKnight
+
+
+class IndexView(generic.ListView):
+    template_name = 'stronaK/index.html'
+    context_object_name = 'last_news'
+
+    def get_queryset(self):
+        """Zwraca najnowsze aktualności"""
+        return News.objects.order_by('date')
 
 
 def index(request):
@@ -17,6 +28,15 @@ def index(request):
     return render(request, 'stronaK/index.html', context)
 
 
+class ListOfSongView(generic.ListView):
+    template_name = "stronaK/spiewnik.html"
+    context_object_name = 'list_of_song'
+
+    def get_queryset(self):
+        """Zwraca wszystkie piosenki"""
+        return Song.objects.order_by('title')
+
+
 def list_of_song(request):
     """
     czytamy z bazy piosenki i wyświetlamy po jednej na stronę,
@@ -25,6 +45,15 @@ def list_of_song(request):
     """
 
     return render(request, 'stronaK/spiewnik.html')
+
+
+class ListOfOldView(generic.ListView):
+    template_name = 'stronaK/zmarli.html'
+    context_object_name = 'list_of_old'
+
+    def get_queryset(self):
+        """ Zwraca listę znanych starych Filistrów """
+        return OldKnight.objects.order_by("surname")
 
 
 def list_of_old(request):
@@ -37,11 +66,19 @@ def list_of_old(request):
     return render(request, 'stronaK/zmarli.html', context)
 
 
+class StoryOfUsView(generic.DetailView):
+    template_name = 'stronaK/historiaKujawji.html'
+
+
 def story_of_us(request):
     """
     Historia Kujawji wpisana na sztywno w HTMLa.
     """
     return render(request, 'stronaK/historiaKujawji.html')
+
+
+class StoryOfCorporationsView(generic.DetailView):
+    template_name = 'stronaK/historiaKorporacji.html'
 
 
 def story_of_corporations(request):
